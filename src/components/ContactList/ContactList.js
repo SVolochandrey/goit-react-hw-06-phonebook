c8fa962149a4b143bdc95aa6b86c5546e5cbf1e2
SVolochandrey <volochan.andrey@gmail.com>
2023-05-09 15:27:48 +0300

    hw 6

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,30 +1,35 @@
-import PropTypes from 'prop-types';
 import './ContactList.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { getContacts, getFilter, deleteContact } from '../../redux/slice';
 
-export const ContactList = ({ contacts, deleteContact }) => (
-<ul>
-{contacts.map(({ id, name, number }) => {
-return (
-<li key={id}>
-<p>
-{name}: {number}
-</p>
-<button type="button" onClick={() => deleteContact(id)}>
-Delete
-</button>
-</li>
-);
-})}
-</ul>
-);
+export const ContactList = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
 
-ContactList.propTypes = {
-contacts: PropTypes.arrayOf(
-PropTypes.shape({
-id: PropTypes.string.isRequired,
-name: PropTypes.string.isRequired,
-number: PropTypes.string.isRequired,
-})
-).isRequired,
-deleteContact: PropTypes.func.isRequired,
+  const findContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const filteredContacts = findContacts();
+
+  return (
+    <ul>
+      {filteredContacts.map(({ id, name, number }) => {
+        return (
+          <li key={id}>
+            <p>
+              {name}: {number}
+            </p>
+            <button type="button" onClick={() => dispatch(deleteContact(id))}>
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,18 +1,23 @@
-import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { setFilter, getFilter } from '../../redux/slice';
 import './Filter.module.css';
 
-export const Filter = ({ filter, changeFilterInput }) => (
-<label>
-<input
-type="text"
-name={filter}
-onChange={changeFilterInput}
-placeholder="Find contacts by name"
-/>
-</label>
-);
+export const Filter = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector(getFilter);
 
-Filter.propTypes = {
-filter: PropTypes.string.isRequired,
-changeFilterInput: PropTypes.func.isRequired,
+  const onChangeFilter = evt => {
+    dispatch(setFilter(evt.target.value));
+  };
+
+  return (
+    <label>
+      <input
+        type="text"
+        name={filter}
+        onChange={onChangeFilter}
+        placeholder="Find contacts by name"
+      />
+    </label>
+  );
 };
